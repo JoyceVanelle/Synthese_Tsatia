c0ef6541fac40f15a7c205ce10f55e1eca907b80
Joyce Vanelle Tsatia Momo <joyce.vanelle.tsatia.momo.01@edu.cegeptr.qc.ca>
2023-04-25 13:52:16 -0400

    Initial commit

---
+++ Assets/_MyAssets/Scripts/Jouer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jouer : MonoBehaviour
+{
+
+    [SerializeField] private float _vitesse = 10.0f;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouvementsJoueur();
+    }
+
+    private void MouvementsJoueur()
+    {
+        float horizInput = Input.GetAxis("Horizontal");
+        float vertiInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizInput, vertiInput, 0f);
+        transform.Translate(direction * Time.deltaTime * _vitesse);
+        // ajuster la position en y (vertical) en utilisant le math.clamp
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 3.5f), 0f);
+        //  ajuster la position en x
+        if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0f);
+        }
+        else if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0f);
+        }
+    }
+}
+
