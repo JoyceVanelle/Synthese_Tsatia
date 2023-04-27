829f5a8d42daf2e9b2e141bb2e23cf3a6b76c59b
Joyce Vanelle Tsatia Momo <joyce.vanelle.tsatia.momo.01@edu.cegeptr.qc.ca>
2023-04-27 13:00:17 -0400

    Update Enemie.cs
    pas fini

--- Assets/_MyAssets/Scripts/Enemie.cs
+++ Assets/_MyAssets/Scripts/Enemie.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemie : MonoBehaviour
 {
+    [SerializeField] private float _vitesse = 6f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class Enemie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * Time.deltaTime * _vitesse);
+        if (transform.position.y < -7f)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            transform.position = new Vector3(randomX, 7f, 0f);
+        }
     }
 }
