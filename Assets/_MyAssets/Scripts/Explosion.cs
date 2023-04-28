e0f29dcdd5466b15af7acdde2e15df33ece169f7
Joyce Vanelle Tsatia Momo <joyce.vanelle.tsatia.momo.01@edu.cegeptr.qc.ca>
2023-04-28 14:41:40 -0400

    good
    hello

---
+++ Assets/_MyAssets/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private AudioClip _sonExplosion = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Destroy(gameObject, 0.2f);
+    }
+
+    private void Update()
+    {
+       // AudioSource.PlayClipAtPoint(_sonExplosion, Camera.main.transform.position, 0.2f);
+        transform.Translate(Vector3.down * Time.deltaTime * 2f);
+    }
+}
