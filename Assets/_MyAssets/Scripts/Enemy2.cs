fa26c736d592ab359fb8073a4164e8cfd89e9bc2
Joyce Vanelle Tsatia Momo <joyce.vanelle.tsatia.momo.01@edu.cegeptr.qc.ca>
2023-05-02 13:59:44 -0400

    ji
    good

--- Assets/_MyAssets/Scripts/Enemy2.cs
+++ Assets/_MyAssets/Scripts/Enemy2.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy2 : MonoBehaviour
 {
     [SerializeField] private float _vitesse = 10.0f;
+    [SerializeField] private int _points = 100;
+    [SerializeField] private GameObject _explosionPrefab = default;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,25 +16,30 @@ public class Enemy2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * _vitesse);
+        if (transform.position.x < -7f)
+        {
+            float randomX = Random.Range(-7.0f, 7.0f);
+            transform.position = new Vector3(randomX, 7f, 0f);
+        }
     }
 
-    private void MouvementsJoueur()
-    {
-        float horizInput = Input.GetAxis("Horizontal");
-        float vertiInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizInput, vertiInput, 0f);
-        transform.Translate(direction * Time.deltaTime * _vitesse);
-        // ajuster la position en y (vertical) en utilisant le math.clamp
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 3.5f), 0f);
-        //  ajuster la position en x
-        if (transform.position.x < -9.5f)
-        {
-            transform.position = new Vector3(9.5f, transform.position.y, 0f);
-        }
-        else if (transform.position.x > 9.5f)
-        {
-            transform.position = new Vector3(-9.5f, transform.position.y, 0f);
-        }
-    }
+    //private void MouvementsJoueur()
+    //{
+    //    float horizInput = Input.GetAxis("Horizontal");
+    //    float vertiInput = Input.GetAxis("Vertical");
+    //    Vector3 direction = new Vector3(horizInput, vertiInput, 0f);
+    //    transform.Translate(direction * Time.deltaTime * _vitesse);
+    //    // ajuster la position en y (vertical) en utilisant le math.clamp
+    //    transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 3.5f), 0f);
+    //    //  ajuster la position en x
+    //    if (transform.position.x < -9.5f)
+    //    {
+    //        transform.position = new Vector3(9.5f, transform.position.y, 0f);
+    //    }
+    //    else if (transform.position.x > 9.5f)
+    //    {
+    //        transform.position = new Vector3(-9.5f, transform.position.y, 0f);
+    //    }
+    //}
 }
--- Assets/_MyAssets/Scripts/SpawnManager.cs
+++ Assets/_MyAssets/Scripts/SpawnManager.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject _prefabEnemy = default;
+    [SerializeField] private GameObject _prefabEnemy2 = default;
     [SerializeField] private GameObject _enemyContainer = default;
+    [SerializeField] private GameObject _enemyContainer2 = default;
     [SerializeField] private GameObject[] _listePUPrefabs = default;
     private bool _stopSpawn = false;
+    private bool _stopSpawn2 = false;
     // Start is called before the first frame update
     public void Start()
     {
         StartCoroutine(SpawnEnemyRouting());
+        StartCoroutine(SpawnEnemy2Routing());
     }
 
     // Update is called once per frame
@@ -29,9 +33,22 @@ public class SpawnManager : MonoBehaviour
         }
 
     }
-
+    IEnumerator SpawnEnemy2Routing()
+    {
+        yield return new WaitForSeconds(3.0f);
+        while (_stopSpawn2 == false)
+        {
+            //Vector3 posiSpawn = new Vector3(Random.Range(-7.0f, 7.0f), -0.5f, 0f);
+            Vector3 posiSpawn = new Vector3(7.0f, -3f, 0f);
+            GameObject newEnemy2 = Instantiate(_prefabEnemy2, posiSpawn, Quaternion.identity);
+            newEnemy2.transform.parent = _enemyContainer2.transform;
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    
     public void FinJeu()
     {
         _stopSpawn = true;
+        _stopSpawn2 = true;
     }
 }
